bbab169d30998cc39f1906b83d15a99fae585d5f
mattsan83 <cohenmatthieu1@gmail.com>
2022-11-06 18:05:49 +0100

    modif

--- connexion.php
+++ connexion.php
@@ -154,7 +154,7 @@
 
 
        <div style="width: 100%;">
-           <h1 class="head_title secondary">Connexion </h1>
+           <h1 class="head_title git pushsecondary">Connexion </h1>
        </div>>
 
        <div style="display: bloc; height: 70vh; flex-wrap: wrap; align-items: center; justify-content: center; align-content: flex-start;">
