0b26c23dc25c940b0ef067eccb9cc1cf87ebbfa4
unknown <alexandremaugy@gmail.com>
2022-11-13 11:41:32 +0100

    Mise à jour du responsive de la page Program

--- program.php
+++ program.php
@@ -36,7 +36,7 @@ if (isset($_SESSION["PlayerId"])) {
     <?php
     include_once './includes/head.php';
     ?>
-    <link rel="stylesheet" href="css/program_css.css" />
+    <link rel="stylesheet" href="css/programCss.css" />
 
 </head>
 
@@ -75,7 +75,9 @@ if (isset($_SESSION["PlayerId"])) {
                     <h1 class="head_title primary titre-lan">MMI LAN</h1>
                     <h2 class="date-lan">15/12/2022 à 08:00</h2>
                 </div>
-                <p class="txt-lan">La MMI LAN, c’est un événement qui rassemble les joueurs de jeux vidéos faisant parti de la promotion MMI à l’université de Toulon. Une première édition a eu lieu en 2020, en distanciel à cause du Covid-19. Cette année, nous voulons faire une LAN en présentiel, qui se déroulera à l’université de Toulon le jeudi 15 décembre. Les participants s’affronteront sur 6 jeux, et un jeu surprise est réservé pour la finale. On vous attend nombreux !</p>
+                <div class="lan-explication">
+                    <p class="txt-lan">La MMI LAN, c’est un événement qui rassemble les joueurs de jeux vidéos faisant parti de la promotion MMI à l’université de Toulon. Une première édition a eu lieu en 2020, en distanciel à cause du Covid-19. Cette année, nous voulons faire une LAN en présentiel, qui se déroulera à l’université de Toulon le jeudi 15 décembre. Les participants s’affronteront sur 6 jeux, et un jeu surprise est réservé pour la finale. On vous attend nombreux !</p>
+                </div> 
             </div>
         </div>
     </section>
@@ -89,7 +91,7 @@ if (isset($_SESSION["PlayerId"])) {
         </div>
         <div id="3" class="infos-position">
             <div class="gmaps">
-                <iframe class="gmaps-effect" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1456.1052495917631!2d5.939343960184752!3d43.1211040238023!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12c91b0a14bfe843%3A0x6bcfee881156660f!2sUniversit%C3%A9%20de%20Toulon%20Campus%20Porte%20d%E2%80%99Italie%20Toulon!5e0!3m2!1sfr!2sfr!4v1668156066411!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe class="gmaps-effect" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1456.1052495917631!2d5.939343960184752!3d43.1211040238023!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12c91b0a14bfe843%3A0x6bcfee881156660f!2sUniversit%C3%A9%20de%20Toulon%20Campus%20Porte%20d%E2%80%99Italie%20Toulon!5e0!3m2!1sfr!2sfr!4v1668156066411!5m2!1sfr!2sfr"  style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
             <div class="position">
                 <div class="lan-position">
