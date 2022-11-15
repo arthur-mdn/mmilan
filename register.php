174ed81904fb9d89185a908a4686a254c1436311
Mathis Lambert <mathis.lambert27@gmail.com>
2022-11-15 21:54:37 +0100

    big update on register system

--- register.php
+++ register.php
@@ -60,126 +60,137 @@
     $are_not_empty = (!empty($_POST['NomUtilisateur']) &&  !empty($_POST['PrenomUtilisateur']) &&  !empty($_POST['MailUtilisateur']) &&  !empty($_POST['TelUtilisateur']) &&  !empty($_POST['MdpUtilisateur']) &&  !empty($_POST['DiscordUtilisateur']) &&  !empty($_POST['ProfilUtilisateur']) &&  !empty($_POST['UsernameUtilisateur']));
 
     if ($are_all_set && $are_not_empty) {
-        $query = $conn2->prepare("SELECT count(*) as NombreUtilisateurs
+        $checkNumOfPlayers = $conn2->prepare("SELECT COUNT(*) FROM players WHERE players.PlayerStatus = 'ok' AND players.PlayerRole NOT IN ('admin', 'staff')");
+        $checkNumOfPlayers->execute();
+        $numOfPlayers = $checkNumOfPlayers->fetchColumn();
+        if ($numOfPlayers >= 60) {
+            echo "<script>window.location.href = 'register?full=true';</script>";
+        } else {
+            if ($_POST['ProfilUtilisateur'] === "mmi1") {
+                $query = $conn2->prepare("SELECT count(*) as NombreUtilisateurs
                         FROM players 
                         WHERE (players.PlayerEmail = ? or players.PlayerTel = ?) ");
-        $query->bindValue(1, $_POST['MailUtilisateur']);
-        $query->bindValue(2, $_POST['TelUtilisateur']);
-        $query->execute();
-        $result = $query->fetchAll(PDO::FETCH_ASSOC);
-        if (isset($result[0]) and $result[0]['NombreUtilisateurs'] == 0) {
-            $result = $result[0];
-            $query = "SELECT max(PlayerId) + 1 as NewPlayerId
+                $query->bindValue(1, $_POST['MailUtilisateur']);
+                $query->bindValue(2, $_POST['TelUtilisateur']);
+                $query->execute();
+                $result = $query->fetchAll(PDO::FETCH_ASSOC);
+                if (isset($result[0]) and $result[0]['NombreUtilisateurs'] == 0) {
+                    $result = $result[0];
+                    $query = "SELECT max(PlayerId) + 1 as NewPlayerId
         FROM players";
-            $result2 = $conn2->query($query)->fetch(); // look for the highest number of PlayerId and add 1. ==> Home-made Auto-Increment
+                    $result2 = $conn2->query($query)->fetch(); // look for the highest number of PlayerId and add 1. ==> Home-made Auto-Increment
 
-            // check if game selected exists
-            $check_games = $conn2->prepare("SELECT count(*) as NombreJeux
+                    // check if game selected exists
+                    $check_games = $conn2->prepare("SELECT count(*) as NombreJeux
                         FROM games 
                         WHERE games.GameId = ? ");
-            $check_games->bindValue(1, intval($_POST['FavGameUtilisateur']));
-            $check_games->execute();
-            $result3 = $check_games->fetchAll(PDO::FETCH_ASSOC);
+                    $check_games->bindValue(1, intval($_POST['FavGameUtilisateur']));
+                    $check_games->execute();
+                    $result3 = $check_games->fetchAll(PDO::FETCH_ASSOC);
 
-            if ($result3[0]['NombreJeux'] == 1) {
-                // if there is no player in the database, the first player will have the id 1
-                if (!isset($result2['NewPlayerId'])) {
-                    $result2['NewPlayerId'] = 1;
-                }
+                    if ($result3[0]['NombreJeux'] == 1) {
+                        // if there is no player in the database, the first player will have the id 1
+                        if (!isset($result2['NewPlayerId'])) {
+                            $result2['NewPlayerId'] = 1;
+                        }
 
-                // check if the profile is allowed
-                $profile_allowed = ['mmi1', 'mmi2', 'enseignant', 'other'];
-                if (!in_array($_POST['ProfilUtilisateur'], $profile_allowed)) {
-                    $_POST['ProfilUtilisateur'] = 'other';
-                }
+                        // check if the profile is allowed
+                        $profile_allowed = ['mmi1', 'mmi2', 'enseignant', 'other'];
+                        if (!in_array($_POST['ProfilUtilisateur'], $profile_allowed)) {
+                            $_POST['ProfilUtilisateur'] = 'other';
+                        }
 
-                // Log account creation attempt
-                $logconn = $conn2->prepare("INSERT INTO logs (LogMsg, LogUserMail) 
+                        // Log account creation attempt
+                        $logconn = $conn2->prepare("INSERT INTO logs (LogMsg, LogUserMail) 
                     VALUES (?, ?)");
-                $logconn->bindValue(1, "Account creation : " . $_POST['MailUtilisateur'] . " successfully created its account");
-                $logconn->bindValue(2, $_POST['MailUtilisateur']);
-                $logconn->execute();
+                        $logconn->bindValue(1, "Account creation : " . $_POST['MailUtilisateur'] . " successfully created its account");
+                        $logconn->bindValue(2, $_POST['MailUtilisateur']);
+                        $logconn->execute();
 
-                // create the account - insert the player in the database
-                $query = $conn2->prepare("INSERT INTO players (PlayerId, PlayerLastname, PlayerFirstname, PlayerEmail, PlayerTel, PlayerPassword, PlayerCreation_date, PlayerDiscord, PlayerProfil,PlayerUsername, PlayerFavGameId) 
+                        // create the account - insert the player in the database
+                        $query = $conn2->prepare("INSERT INTO players (PlayerId, PlayerLastname, PlayerFirstname, PlayerEmail, PlayerTel, PlayerPassword, PlayerCreation_date, PlayerDiscord, PlayerProfil,PlayerUsername, PlayerFavGameId) 
                     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?,?, ?)");
-                $query->bindValue(1, $result2['NewPlayerId']);
-                $query->bindValue(2, htmlspecialchars($_POST['NomUtilisateur'], ENT_QUOTES, 'UTF-8'));
-                $query->bindValue(3, htmlspecialchars($_POST['PrenomUtilisateur'], ENT_QUOTES, 'UTF-8'));
-                $query->bindValue(4, htmlspecialchars($_POST['MailUtilisateur'], ENT_QUOTES, 'UTF-8'));
-                $query->bindValue(5, htmlspecialchars($_POST['TelUtilisateur'], ENT_QUOTES, 'UTF-8'));
-                $query->bindValue(6, password_hash($_POST['MdpUtilisateur'], PASSWORD_DEFAULT));
-                $query->bindValue(7, date('Y-m-d H:i:s'));
-                $query->bindValue(8, htmlspecialchars($_POST['DiscordUtilisateur'], ENT_QUOTES, 'UTF-8'));
-                $query->bindValue(9, htmlspecialchars($_POST['ProfilUtilisateur'], ENT_QUOTES, 'UTF-8'));
-                $query->bindValue(10, htmlspecialchars($_POST['UsernameUtilisateur'], ENT_QUOTES, 'UTF-8'));
-                $query->bindValue(11, htmlspecialchars($_POST['FavGameUtilisateur'], ENT_QUOTES, 'UTF-8'));
-                $query->execute(); // create user
-                $result = $query->fetchAll(PDO::FETCH_ASSOC);
+                        $query->bindValue(1, $result2['NewPlayerId']);
+                        $query->bindValue(2, htmlspecialchars($_POST['NomUtilisateur'], ENT_QUOTES, 'UTF-8'));
+                        $query->bindValue(3, htmlspecialchars($_POST['PrenomUtilisateur'], ENT_QUOTES, 'UTF-8'));
+                        $query->bindValue(4, htmlspecialchars($_POST['MailUtilisateur'], ENT_QUOTES, 'UTF-8'));
+                        $query->bindValue(5, htmlspecialchars($_POST['TelUtilisateur'], ENT_QUOTES, 'UTF-8'));
+                        $query->bindValue(6, password_hash($_POST['MdpUtilisateur'], PASSWORD_DEFAULT));
+                        $query->bindValue(7, date('Y-m-d H:i:s'));
+                        $query->bindValue(8, htmlspecialchars($_POST['DiscordUtilisateur'], ENT_QUOTES, 'UTF-8'));
+                        $query->bindValue(9, htmlspecialchars($_POST['ProfilUtilisateur'], ENT_QUOTES, 'UTF-8'));
+                        $query->bindValue(10, htmlspecialchars($_POST['UsernameUtilisateur'], ENT_QUOTES, 'UTF-8'));
+                        $query->bindValue(11, htmlspecialchars($_POST['FavGameUtilisateur'], ENT_QUOTES, 'UTF-8'));
+                        $query->execute(); // create user
+                        $result = $query->fetchAll(PDO::FETCH_ASSOC);
 
-                // join the team if tokens and id are set
-                if (isset($_POST['JoinId']) and isset($_POST['JoinToken'])) {
-                    // check if the invitation exists
-                    $checkInvitationStatus = $conn2->prepare("SELECT InvitationStatus
+                        // join the team if tokens and id are set
+                        if (isset($_POST['JoinId']) and isset($_POST['JoinToken'])) {
+                            // check if the invitation exists
+                            $checkInvitationStatus = $conn2->prepare("SELECT InvitationStatus
                         FROM invitations 
                         WHERE InvitationId = ? and InvitationToken = ?");
-                    $checkInvitationStatus->bindValue(1, htmlspecialchars($_POST['JoinId'], ENT_QUOTES, 'UTF-8'));
-                    $checkInvitationStatus->bindValue(2, htmlspecialchars($_POST['JoinToken'], ENT_QUOTES, 'UTF-8'));
-                    $checkInvitationStatus->execute();
-                    $resultInvitationStatus = $checkInvitationStatus->fetchAll(PDO::FETCH_ASSOC);
+                            $checkInvitationStatus->bindValue(1, htmlspecialchars($_POST['JoinId'], ENT_QUOTES, 'UTF-8'));
+                            $checkInvitationStatus->bindValue(2, htmlspecialchars($_POST['JoinToken'], ENT_QUOTES, 'UTF-8'));
+                            $checkInvitationStatus->execute();
+                            $resultInvitationStatus = $checkInvitationStatus->fetchAll(PDO::FETCH_ASSOC);
 
-                    if ($resultInvitationStatus[0]['InvitationStatus'] != 'En cours') {
-                        header('Location: register.php?error=Cette-invitation-n\'est-plus-valide');
-                    } else if ($resultInvitationStatus[0]['InvitationStatus'] == 'En cours') {
-                        $getTeamId = $conn2->prepare("SELECT InvitationTeamId
+                            if ($resultInvitationStatus[0]['InvitationStatus'] != 'En cours') {
+                                header('Location: register.php?error=Cette-invitation-n\'est-plus-valide');
+                            } else if ($resultInvitationStatus[0]['InvitationStatus'] == 'En cours') {
+                                $getTeamId = $conn2->prepare("SELECT InvitationTeamId
                         FROM invitations 
                         WHERE InvitationId = ? and InvitationToken = ?");
-                        $getTeamId->bindValue(1, $invitationId);
-                        $getTeamId->bindValue(2, $invitationToken);
-                        $getTeamId->execute();
-                        $resultTeamId = $getTeamId->fetchAll(PDO::FETCH_ASSOC);
+                                $getTeamId->bindValue(1, $invitationId);
+                                $getTeamId->bindValue(2, $invitationToken);
+                                $getTeamId->execute();
+                                $resultTeamId = $getTeamId->fetchAll(PDO::FETCH_ASSOC);
 
 
-                        $query = "SELECT IFNULL(MAX(AppartientId), 0) + 1 as NewAppartientId FROM appartient";
-                        $NewAppartientId = $conn2->query($query)->fetch(); // look for the highest number of TeamId and add 1. ==> Home-made Auto-Increment;
+                                $query = "SELECT IFNULL(MAX(AppartientId), 0) + 1 as NewAppartientId FROM appartient";
+                                $NewAppartientId = $conn2->query($query)->fetch(); // look for the highest number of TeamId and add 1. ==> Home-made Auto-Increment;
 
-                        $query = $conn2->prepare("UPDATE invitations
+                                $query = $conn2->prepare("UPDATE invitations
                                             SET InvitationStatus = 'AcceptÃƒÂ©e'
                                             WHERE InvitationId = ?
                                             AND InvitationToken = ?");
 
-                        $query->bindValue(1, $invitationId);
-                        $query->bindValue(2, $invitationToken);
-                        $query->execute();
+                                $query->bindValue(1, $invitationId);
+                                $query->bindValue(2, $invitationToken);
+                                $query->execute();
 
-                        $query = $conn2->prepare("INSERT INTO appartient (AppartientId,AppartientPlayerId, AppartientTeamId, AppartientRole)
-                                            VALUES (?, ?, ?, 'joueur')
+                                $query = $conn2->prepare("INSERT INTO appartient (AppartientId,AppartientPlayerId, AppartientTeamId, AppartientRole)
+                                            VALUES (?, ?, ?, 'player')
                                             ");
-                        $query->bindValue(1, $NewAppartientId['NewAppartientId']);
-                        $query->bindValue(2, $result2['NewPlayerId']);
-                        $query->bindValue(3, $resultTeamId[0]['InvitationTeamId']);
-                        $query->execute();
-                    }
-                }
+                                $query->bindValue(1, $NewAppartientId['NewAppartientId']);
+                                $query->bindValue(2, $result2['NewPlayerId']);
+                                $query->bindValue(3, $resultTeamId[0]['InvitationTeamId']);
+                                $query->execute();
+                            }
+                        }
 
-                // new session for the new player
-                $_SESSION["PlayerId"] = $result2['NewPlayerId'];
-                $_SESSION['PlayerMail'] = $_POST['MailUtilisateur'];
+                        // new session for the new player
+                        $_SESSION["PlayerId"] = $result2['NewPlayerId'];
+                        $_SESSION['PlayerMail'] = $_POST['MailUtilisateur'];
 
 
-                echo '<script type="text/javascript">window.location = "index.php"</script>';
-            } else {
-                // if the game selected does not exist, log hack attempt
-                $logHack = $conn2->prepare("INSERT INTO logs (LogMsg, LogUserMail) 
+                        echo '<script type="text/javascript">window.location = "index.php"</script>';
+                    } else {
+                        // if the game selected does not exist, log hack attempt
+                        $logHack = $conn2->prepare("INSERT INTO logs (LogMsg, LogUserMail) 
                     VALUES (?, ?)");
-                $logHack->bindValue(1, "Hack attempt : " . $_POST['FavGameUtilisateur'] . " is not a valid game");
-                $logHack->bindValue(2, $_POST['MailUtilisateur']);
-                $logHack->execute();
-                header("location: register.php?hack_log=true");
+                        $logHack->bindValue(1, "Hack attempt : " . $_POST['FavGameUtilisateur'] . " is not a valid game");
+                        $logHack->bindValue(2, $_POST['MailUtilisateur']);
+                        $logHack->execute();
+                        header("location: register.php?hack_log=true");
+                    }
+                } else {
+                    $generated_id = generateRandomString(5);
+                    echo '<div class="modal error" id="modal_' . $generated_id . '" onclick="close_modal(\'' . $generated_id . '\')"> Un compte utilisant cette adresse mail et/ou ce numÃƒÂ©ro de tÃƒÂ©lÃƒÂ©phone existe dÃƒÂ©jÃƒÂ . <script> hideIt("modal_' . $generated_id . '"); </script> </div>';
+                }
+            } else {
+                echo "<script>document.location.href = 'register.php?error=mmi1-only'</script>";
             }
-        } else {
-            $generated_id = generateRandomString(5);
-            echo '<div class="modal error" id="modal_' . $generated_id . '" onclick="close_modal(\'' . $generated_id . '\')"> Un compte utilisant cette adresse mail et/ou ce numÃƒÂ©ro de tÃƒÂ©lÃƒÂ©phone existe dÃƒÂ©jÃƒÂ . <script> hideIt("modal_' . $generated_id . '"); </script> </div>';
         }
     } else {
         if (!empty($_POST)) {
@@ -189,14 +200,6 @@
     }
     ?>
     <section id="inscription">
-        <!--         <div class="triangle_1" id="forme_1">
-            <img src="Elements/others/TriangleJB.svg" />
-        </div>
-        <div class="triangle_2">
-            <img src="Elements/others/TriangleJB.svg" />
-        </div> -->
-
-
         <div class="inscription_form_container">
             <h2 class="head_title primary">Inscription Solo</h2>
             <form method="post" onsubmit="active_loader()">
