ddafc77b50604e3198d5b19a2d8dba3fb621efda
mattsan83 <cohenmatthieu1@gmail.com>
2022-11-19 13:14:59 +0100

    création contact.php

---
+++ contact.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * mmilan, website that manage e-sport teams
+ * Propulsed by Arthur Mondon.
+ *
+ * @author     Arthur Mondon
+ *
+ * Contributors :
+ * -Kylian Diochon
+ * -Matthieu Cohen
+ *
+ */
+?>
+<!DOCTYPE html>
+<html lang="fr" xml:lang="fr" xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <title>Accueil</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <script src="js/main_script.js"></script>
+    <link rel="icon" type="image/png" href="Elements/placeholder_logo.svg" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/loader.css" />
+</head>
+
+<body class="body-panel">
+    <div class="frise_container">
+        <div class="frise">
+            <img src="Elements/others/Vector.svg" />
+        </div>
+    </div>
+
+    <div class="loader_container" id="loader_container">
+        <div class="a">
+            <div></div>
+            <div></div>
+        </div>
+        <p>Un instant ...</p>
+        <script>
+            function active_loader() {
+                document.getElementById('loader_container').style.display = 'flex';
+            }
+        </script>
+    </div>
+    <?php
+    require('menu.php'); // afficher le menu en fonction de connecté ou pas.
+    ?>
+</body>
+
+</html>
