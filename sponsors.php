71aef18866c8e7408294d17ce8152d411d67d50b
Mathis Lambert <mathis.lambert27@gmail.com>
2022-12-07 18:40:24 +0100

    fix link viewer on sponsors

--- sponsors.php
+++ sponsors.php
@@ -51,79 +51,61 @@ if (isset($_SESSION["PlayerId"])) {
             <li><a href="#1" class="active">1</a></li>
             <li><a href="#2">2</a></li>
             <li><a href="#3">3</a></li>
+            <li><a href="#4">4</a></li>
             <div class="cursor"></div>
         </ul>
-        <script>
-            const cursor = document.querySelector(".cursor");
-            const links_viewer = document.querySelectorAll(
-                ".section-viewer ul li a"
-            );
-            const sectionWidth = links_viewer[0].getBoundingClientRect().height;
-
-            links_viewer.forEach((link, index) => {
-                console.log(index, sectionWidth);
-                link.addEventListener("click", () => {
-                    links_viewer.forEach((link) => {
-                        link.classList.remove("active");
-                    });
-                    link.classList.add("active");
-                    cursor.style.top = index * sectionWidth + "px";
-                });
-            });
-        </script>
     </div>
-    <section>
-        <h1 class="head_title primary sponsor-title">Nos sponsors</h1>
-        <div id="1" class="sponsor-row">
-            <div class="sponsor-img">
-                <img src="Elements/sponsors/ekinsport_logo_color.svg" alt="Logo d'Ekinsport">
-            </div>
-            <div class="sponsor-txt">
-                <p class="nom-sponsor">Ekinsport</p>
-                <p class="sponsor-exp">Ils nous ont offert des T-Shirt pour pouvoir représenter le staff le jour de l'évènement.</p>
-            </div>
-            <div class="el_1">
-                <img src="Elements/others/01blue.svg" alt="Numéro O1 Bleu">
-            </div>
-        </div>
-        <div id="2" class="sponsor-row-reverse">
-            <div class="sponsor-img">
-                <img src="Elements/sponsors/goodfood_logo_color.svg" alt="Logo de GoodFood">
-            </div>
-            <div class="sponsor-txt">
-                <p class="nom-sponsor">GoodFood</p>
-                <p class="sponsor-exp-reverse">Ils nous ont offert des prix pour les gagnants de notre lan.</p>
-            </div>
-            <div class="el_1">
-                <img src="Elements/others/02White.svg" alt="Numéro O2 Blanc">
-            </div>
-        </div>
-        <div id="3" class="sponsor-row">
-            <div class="sponsor-img">
-                <img src="Elements/sponsors/podium_logo_color.svg" alt="Logo de Podium">
-            </div>
-            <div class="sponsor-txt">
-                <p class="nom-sponsor">Podium</p>
-                <p class="sponsor-exp">Ils nous ont offert les trophées personalisés pour les gagnants.</p>
-            </div>
-            <div class="el_1">
-                <img src="Elements/others/03blue.svg" alt="Numéro O3 Bleu">
-            </div>
-        </div>
-        <div id="4" class="sponsor-row-reverse">
-            <div class="sponsor-img">
-                <img src="Elements/sponsors/rct_logo_color.svg" alt="Logo du RCT">
-            </div>
-            <div class="sponsor-txt">
-                <p class="nom-sponsor">RCT</p>
-                <p class="sponsor-exp-reverse">Ils nous ont offert des prix pour les gagnants de notre lan.</p>
-            </div>
-            <div class="el_1">
-                <img src="Elements/others/04White.svg" alt="Numéro O4 Blanc">
-            </div>
-        </div>
 
+    <h1 class="head_title primary sponsor-title">Nos sponsors</h1>
+    <section id="1" class="sponsor-row">
+        <div class="sponsor-img">
+            <img src="Elements/sponsors/ekinsport_logo_color.svg" alt="Logo d'Ekinsport">
+        </div>
+        <div class="sponsor-txt">
+            <p class="nom-sponsor">Ekinsport</p>
+            <p class="sponsor-exp">Ils nous ont offert des T-Shirt pour pouvoir représenter le staff le jour de l'évènement.</p>
+        </div>
+        <div class="el_1">
+            <img src="Elements/others/01blue.svg" alt="Numéro O1 Bleu">
+        </div>
     </section>
+    <section id="2" class="sponsor-row-reverse">
+        <div class="sponsor-img">
+            <img src="Elements/sponsors/goodfood_logo_color.svg" alt="Logo de GoodFood">
+        </div>
+        <div class="sponsor-txt">
+            <p class="nom-sponsor">GoodFood</p>
+            <p class="sponsor-exp-reverse">Ils nous ont offert des prix pour les gagnants de notre lan.</p>
+        </div>
+        <div class="el_1">
+            <img src="Elements/others/02White.svg" alt="Numéro O2 Blanc">
+        </div>
+    </section>
+    <section id="3" class="sponsor-row">
+        <div class="sponsor-img">
+            <img src="Elements/sponsors/podium_logo_color.svg" alt="Logo de Podium">
+        </div>
+        <div class="sponsor-txt">
+            <p class="nom-sponsor">Podium</p>
+            <p class="sponsor-exp">Ils nous ont offert les trophées personalisés pour les gagnants.</p>
+        </div>
+        <div class="el_1">
+            <img src="Elements/others/03blue.svg" alt="Numéro O3 Bleu">
+        </div>
+    </section>
+    <section id="4" class="sponsor-row-reverse">
+        <div class="sponsor-img">
+            <img src="Elements/sponsors/rct_logo_color.svg" alt="Logo du RCT">
+        </div>
+        <div class="sponsor-txt">
+            <p class="nom-sponsor">RCT</p>
+            <p class="sponsor-exp-reverse">Ils nous ont offert des prix pour les gagnants de notre lan.</p>
+        </div>
+        <div class="el_1">
+            <img src="Elements/others/04White.svg" alt="Numéro O4 Blanc">
+        </div>
+    </section>
+
     <?php
     include("includes/footer.php");
     ?>
